36454b0cdcd773dcaa9df213611c7a56841a563e
Sami Alassafin <alassafin@ggmgastro.com>
2022-05-13 12:53:44 +0200

    Add project files.

---
+++ Krammatik Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Krammatik_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
